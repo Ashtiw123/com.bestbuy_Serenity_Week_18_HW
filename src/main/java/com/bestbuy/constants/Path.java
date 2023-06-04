b3758b46f359f6702c1b29499955c214687a87c2
Ashtiw123 <ashtiwh007@gmail.com>
2023-06-05 00:03:46 +0100

    com.bestbuy_Serenity_Week_18_HW

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,11 @@
+package com.bestbuy.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    public static final String PRODUCTS = "/products";
+    public static final String STORES = "/stores";
+}
+
+
